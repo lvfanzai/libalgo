cc17be30c5813e6ea439bbcd841dfbcf9497924b
lvfanzai <476128501@qq.com>
2016-08-27 22:46:18 +0800

    add test arry

--- testcase/testcase.cpp
+++ testcase/testcase.cpp
@@ -1,6 +1,8 @@
 #include <limits.h>
 #include "gtest/gtest.h"
 #include "libalgo.h"
+#include "libcom.h"
+#include "libtest.h"
 
 class MultiplyTest : public testing::Test {
 
@@ -13,9 +15,12 @@ TEST_F(MultiplyTest, findMean)
 }
 TEST_F(MultiplyTest, findMean1)
 {
-	int a;
-	a = print_test();
-	EXPECT_EQ(0, a);
+	int a[8] = {1,4,2,7,2,3,6,7};
+	int result[8] = {1,2,2,3,4,6,7,7};
+	bubble_sort(a, 8);
+	for (int i = 0; i < 8; i++) {
+		EXPECT_EQ(result[i], a[i]);
+	}
 }
 TEST_F(MultiplyTest, findMean2)
 {
